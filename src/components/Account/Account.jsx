f70952593b7f6235b975dedc19c1e2215dbb2d74
Bilal <89836993+Bilal2903@users.noreply.github.com>
2024-05-31 20:31:17 +0200

    Code cleanup

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { StyleSheet, View, StatusBar, Text } from "react-native";
 
-export default function AccountScreen() {}
+export default function AccountScreen() {
+    
+}
