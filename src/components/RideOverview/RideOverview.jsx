cb8aee236a38d38318cd93fa5196f9fb4b964c19
Bilal <89836993+Bilal2903@users.noreply.github.com>
2024-05-31 21:27:33 +0200

    Cleanup

--- src/components/RideOverview/RideOverview.jsx
+++
@@ -1,7 +0,0 @@
-import { StyleSheet, View } from "react-native";
-import MapView, { Marker } from "react-native-maps";
-import React, { useEffect, useState } from "react";
-
-export default function OverviewScreen() {
-    
-}
